39f358162749af8a1a81908a5f83365858bc4c7e
Alone <1293813551@qq.com>
2016-06-29 22:53:52 +0800

    Create config-01.php

---
+++ WeiXin/config-01.php
@@ -0,0 +1,8 @@
+<?php
+// 全局配置
+$G_ROOT = dirname(__FILE__);
+$G_CONFIG["weiXin"] = array(
+        'CorpID' => 'wxfcf60e7eba5f47f3',
+        'Secret' => 'Ud-8Gdb02jUgnmNRJ1Rar0VSoAjJAqsUpBQN15Wl-fk3SwLJFINkSPGz138BBkLD',
+        'agentid' => 4,  //应用ID
+);
